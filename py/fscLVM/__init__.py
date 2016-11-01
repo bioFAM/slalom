6c0cc04bea45e74dd9db9cb045670254b4ef9e50
Flo <fbuettner.phys@gmail.com>
2016-11-01 14:03:29 +0000

    versioning

--- py/fscLVM/__init__.py
+++ py/fscLVM/__init__.py
@@ -1,1 +1,11 @@
+from __future__ import absolute_import as _absolute_import
+
+from pkg_resources import get_distribution as _get_distribution
+from pkg_resources import DistributionNotFound as _DistributionNotFound
+
+try:
+    __version__ = _get_distribution('fscLVM').version
+except _DistributionNotFound:
+    __version__ = 'unknown'
+
 from .utils import (initFA, load_hdf5, plotTerms, plotFactors, saveFA)

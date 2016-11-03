b51e210d13f730cbdd7e2eb6135bdfcad9eecbec
Flo <fbuettner.phys@gmail.com>
2016-11-03 14:04:11 +0000

    update to 3.3

--- py/setup.py
+++ py/setup.py
@@ -20,7 +20,7 @@ def setup_package():
 
    metadata = dict(
        name='fscLVM',
-       version='1.0.0.dev1',
+       version='1.0.0.dev2',
        maintainer="f-scLVM Developers",
        maintainer_email="fbuettner.phys@gmail.com",
        license="Apache 2.0",
